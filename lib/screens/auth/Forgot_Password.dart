29370e9415c24bf5cedaf7d11139f70a146802d4
Ramjinish Kushwaha <zbuks03@gmail.com>
2023-09-13 17:15:43 +0545

    Forgot and remember me screen

---
+++ lib/screens/auth/Forgot_Password.dart
@@ -0,0 +1,239 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'dart:convert';
+
+import 'package:TezHealthCare/utils/My_button.dart';
+import 'package:TezHealthCare/utils/colors.dart';
+import 'package:TezHealthCare/utils/helper_class.dart';
+import 'package:flutter/material.dart';
+import 'package:TezHealthCare/utils/mediaqury.dart';
+import 'package:http/http.dart' as http;
+
+class Forgotpassword extends StatefulWidget {
+  const Forgotpassword({Key? key}) : super(key: key);
+
+  @override
+  State<Forgotpassword> createState() => _ForgotpasswordState();
+}
+
+class _ForgotpasswordState extends State<Forgotpassword> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController EmailController = TextEditingController();
+  final TextEditingController DobController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+  var isloading = false;
+
+  void resetPassword(String email) async {
+    const apiUrl = 'https://uat.tez.hospital/xzy/webservice/forgotpassword';
+    final body = {
+      'username': email
+    }; // You can change 'email' to 'username' based on your API requirements.
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {
+        'Soft-service': 'TezHealthCare',
+        'Auth-key': 'zbuks_ram859553467'
+      },
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 200) {
+      // Password reset request successful
+      // You can handle the success response here
+    } else {
+      // Password reset request failed
+      // You can handle the error response here
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Utils.scaffoldBackgroundColor,
+        appBar: AppBar(
+          title: const Text('Forgot Password'),
+          centerTitle: true,
+          backgroundColor: darkblue,
+          foregroundColor: Utils.appbarForgroundColor,
+        ),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                height: 170,
+                decoration: BoxDecoration(
+                    color: darkblue,
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30))),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Utils.containerColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Form(
+                    key: formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '  Username',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter UserName';
+                              }
+                              //  else if (!value.contains('@')) {
+                              //   return 'please enter valid email';
+                              // }
+                              else {
+                                return null;
+                              }
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            controller: usernameController,
+                            onTapOutside: (event) =>
+                                FocusScope.of(context).unfocus(),
+                            decoration: InputDecoration(
+                                fillColor: Colors.white,
+                                filled: true,
+                                prefixIcon: const Icon(
+                                  Icons.person,
+                                  color: Colors.amber,
+                                ),
+                                hintText: 'Enter Yor Username',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            'Email',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please Enter Your Email';
+                              } else if (!value.contains(
+                                '@',
+                              )) {
+                                return 'please enter valid email';
+                              } else {
+                                return null;
+                              }
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            controller: EmailController,
+                            onTapOutside: (event) =>
+                                FocusScope.of(context).unfocus(),
+                            decoration: InputDecoration(
+                                fillColor: Colors.white,
+                                filled: true,
+                                prefixIcon: const Icon(
+                                  Icons.mail,
+                                  color: Colors.amber,
+                                ),
+                                hintText: 'Enter Your Email',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                         const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            'Date Of Birth',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please Enter Your Dob';
+                              }
+                              // else if (!value.contains(
+                              //   '',
+                              // )) {
+                              //   return 'please enter valid email';
+                              // } else {
+                              //   return null;
+                              // }
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            controller: DobController,
+                            onTapOutside: (event) =>
+                                FocusScope.of(context).unfocus(),
+                            decoration: InputDecoration(
+                                fillColor: Colors.white,
+                                filled: true,
+                                prefixIcon: const Icon(
+                                  Icons.calendar_month,
+                                  color: Colors.amber,
+                                ),
+                                hintText: 'Example:- 2057-01-02 / 2000-04-14',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Container(
+                              width: double.infinity,
+                              height: 50,
+                              child: MyButton(
+                                title: isloading
+                                    ? const CircularProgressIndicator()
+                                    : const Text('Forgot'),
+                                onPressed: () {
+                                  if (formKey.currentState!.validate()) {
+                                    resetPassword;
+                                    setState(() {
+                                      isloading = true;
+                                    });
+                                  }
+                                },
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/auth/Patient_login.dart
+++ lib/screens/auth/Patient_login.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
+import 'package:TezHealthCare/screens/auth/Forgot_Password.dart';
 import 'package:TezHealthCare/utils/colors.dart';
 import 'package:http/http.dart' as http;
 import 'package:TezHealthCare/bottombar/bottombar.dart';
-import 'package:TezHealthCare/bottomscreen/home/forgotpassword.dart';
 import 'package:TezHealthCare/utils/My_button.dart';
 import 'package:TezHealthCare/utils/mediaqury.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +10,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 class PatientLogin extends StatefulWidget {
   const PatientLogin({Key? key}) : super(key: key);
 
@@ -18,6 +19,7 @@ class PatientLogin extends StatefulWidget {
 }
 
 class _PatientLoginState extends State<PatientLogin> {
+  bool _rememberMeFlag = false;
   String id = '';
   bool _isPasswordVisible = false;
   void _togglePasswordVisibility() {
@@ -25,6 +27,7 @@ class _PatientLoginState extends State<PatientLogin> {
       _isPasswordVisible = !_isPasswordVisible;
     });
   }
+
   final TextEditingController _passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
   var isloading = false;
@@ -60,7 +63,8 @@ class _PatientLoginState extends State<PatientLogin> {
       // Navigate to the home screen or any other screen you need
       Get.off(() => const Bottomhome());
       setState(() {
-        Fluttertoast.showToast(msg: 'Login Successfully',
+        Fluttertoast.showToast(
+          msg: 'Login Successfully',
           backgroundColor: Colors.green,
           textColor: Colors.white,
         );
@@ -81,157 +85,186 @@ class _PatientLoginState extends State<PatientLogin> {
   Widget build(BuildContext context) {
     return isloading
         ? Center(
-      child: Lottie.asset('assets/log_load.json'),
-    )
+            child: Lottie.asset('assets/log_load.json'),
+          )
         : SingleChildScrollView(
-      child: Container(
-        margin: const EdgeInsets.all(20),
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 146, 99, 227).withOpacity(0.2),
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              spreadRadius: 5,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: double.infinity,
-              height: height / 5,
-              child: Image.asset('assets/logo.png'),
-            ),
-            Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    '  Username',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter UserName';
-                      }
-                      //  else if (!value.contains('@')) {
-                      //   return 'please enter valid email';
-                      // }
-                      else {
-                        return null;
-                      }
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    controller: usernameController,
-                    onTapOutside: (event) =>
-                        FocusScope.of(context).unfocus(),
-                    decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        prefixIcon: const Icon(Icons.person),
-                        prefixIconColor: yellow,
-                        hintText: 'Enter Username',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    '  Password',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please Enter your password';
-                      } else {
-                        return null;
-                      }
-                    },
-                    controller: _passwordController,
-                    obscureText: !_isPasswordVisible,
-                    onTapOutside: (event) => FocusScope.of(context).unfocus(),
-                    decoration: InputDecoration(
-                      focusColor: yellow,
-                        fillColor: Colors.white,
-                        filled: true,
-                        prefixIcon:  Icon(Icons.lock,color: Color(0xfffabd0a),),
-                        hintText: 'Enter Password',
-                        suffixIconColor: yellow,
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                          ),
-                          onPressed: _togglePasswordVisibility,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )
-                    ),
-                  ),
-
-                  Container(
-                      alignment: Alignment.bottomRight,
-                      child: TextButton(
-                        onPressed: () {
-                          Get.to(() => Forgotpassword());
-                        },
-                        child: const Text(
-                          'Forgot Password',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 50,
-                    child: MyButton(
-                      title: const Text('Sign In'),
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          // Loginvalues.patientLogin(
-                          //   Loginvalues.emailController,
-                          //   Loginvalues.passwordController
-                          // );
-                          _login();
-                          setState(() {
-                            isloading = true;
-                          });
-                        }
-                      },
-                    ),
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 146, 99, 227).withOpacity(0.2),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
                   ),
                 ],
               ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: height / 5,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '  Username',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter UserName';
+                            }
+                            //  else if (!value.contains('@')) {
+                            //   return 'please enter valid email';
+                            // }
+                            else {
+                              return null;
+                            }
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          controller: usernameController,
+                          onTapOutside: (event) =>
+                              FocusScope.of(context).unfocus(),
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              prefixIcon: const Icon(Icons.person),
+                              prefixIconColor: yellow,
+                              hintText: 'Enter Username',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              )),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          '  Password',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please Enter your password';
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: _passwordController,
+                          obscureText: !_isPasswordVisible,
+                          onTapOutside: (event) =>
+                              FocusScope.of(context).unfocus(),
+                          decoration: InputDecoration(
+                              focusColor: yellow,
+                              fillColor: Colors.white,
+                              filled: true,
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Color(0xfffabd0a),
+                              ),
+                              hintText: 'Enter Password',
+                              suffixIconColor: yellow,
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _isPasswordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: _togglePasswordVisibility,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              )),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Checkbox(
+                                  activeColor: yellow,
+                                  focusColor: yellow,
+                                  checkColor: darkblue,
+                                  value: _rememberMeFlag,
+                                  onChanged: (value) => setState(() {
+                                    _rememberMeFlag = !_rememberMeFlag;
+                                  }),
+                                ),
+                                Text(
+                                  'Remember Me',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20.0), // Spacer
+                            InkWell(
+                                onTap: () {
+                                  // Add your Forgot Password functionality here
+                                },
+                                child: TextButton(
+                                  onPressed: () {
+                                    Get.to(() => Forgotpassword());
+                                  },
+                                  child: const Text(
+                                    'Forgot Password',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                )),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 50,
+                          child: MyButton(
+                            title: const Text('Sign In'),
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                // Loginvalues.patientLogin(
+                                //   Loginvalues.emailController,
+                                //   Loginvalues.passwordController
+                                // );
+                                _login();
+                                setState(() {
+                                  isloading = true;
+                                });
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: height / 10,
+                  )
+                ],
+              ),
             ),
-            SizedBox(
-              height: height / 10,
-            )
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
