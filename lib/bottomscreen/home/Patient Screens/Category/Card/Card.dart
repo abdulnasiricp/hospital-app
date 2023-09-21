1bc3e0a4a2a7186854c8bc873ee24d42c96010de
Abdul Nasir <abdulnasiricp@gmail.com>
2023-09-21 16:14:25 +0500

    card display and downlaod

--- lib/bottomscreen/home/Patient Screens/Category/Card/Card.dart
+++ lib/bottomscreen/home/Patient Screens/Category/Card/Card.dart
@@ -1,49 +1,79 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print, file_names, unnecessary_string_interpolations
 
-// ignore_for_file: avoid_unnecessary_containers, file_names
-
+import 'package:TezHealthCare/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
 class CardScreen extends StatefulWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
-  _CardScreenState createState() => _CardScreenState();
+  State<CardScreen> createState() => _CardScreenState();
 }
 
 class _CardScreenState extends State<CardScreen> {
+  double? _progress;
+  String PatientId = '';
+  LoadData() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    PatientId = sp.getString('patientidrecord') ?? '';
+    print(PatientId);
+    setState(() {});
+  }
 
-  double _progress=0;
-  late InAppWebViewController inAppWebViewController;
-  
+  loadSP() async {
+    await LoadData();
+  }
+
+  @override
+  void initState() {
+    loadSP();
+    
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body:Stack(
-          children: [
-            InAppWebView(
-              initialUrlRequest: URLRequest(url: Uri.parse("https://uat.tez.hospital/admin/generatecertificate/generatepatientidcardPdf/10380")),
-              onWebViewCreated: (InAppWebViewController controller){
-                inAppWebViewController= controller;
-              },
-              onProgressChanged: (InAppWebViewController controller,int progress){
-                setState(() {
-                  _progress =progress/100;
-                });
-              },
-            ),
-            _progress<1?Container(
-              child: Center(
-                child: CircularProgressIndicator(
-                  value: _progress,
-                ),
-              ),
-            ):const SizedBox()
-    
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+           _progress!=null?const Center(child: Padding(
+             padding: EdgeInsets.all(8.0),
+             child: CircularProgressIndicator(),
+           )): IconButton(
+                onPressed: () {
+                  FileDownloader.downloadFile(
+                      url:
+                          'https://uat.tez.hospital/xzy/webservice/generateIdcard/$PatientId',
+                      onProgress: (name, progress) {
+                       setState(() {
+                          _progress=progress;
+                       });
+                      },
+                      onDownloadCompleted: (path) {
+                        print('$path');
+                        setState(() {
+                          _progress=null;
+                        });
+                        
+                      }
+                      );
+                },
+                icon: const Icon(Icons.download))
           ],
-        )
-        
-      ),
-    );
-  }}
-    
+          backgroundColor: darkYellow,
+          title: const Text('Card'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SfPdfViewer.network('https://uat.tez.hospital/xzy/webservice/generateIdcard/$PatientId/'
+              
+              // 'https://uat.tez.hospital/xzy/webservice/generateIdcard/10819'
+              ),
+        ));
+  }
+}
