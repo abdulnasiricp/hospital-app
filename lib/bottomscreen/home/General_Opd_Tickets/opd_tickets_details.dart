b076f6ee36922f9893914f793551a5f245fb9890
Abdul Nasir <abdulnasiricp@gmail.com>
2023-11-07 10:15:48 +0500

    updated

--- lib/bottomscreen/home/General_Opd_Tickets/opd_tickets_details.dart
+++ lib/bottomscreen/home/General_Opd_Tickets/opd_tickets_details.dart
@@ -58,7 +58,7 @@ class _ConfirmationScreenState extends State<OPDTicketDetails> {
 
   //convert rupess to paisa
   late num OPdRupeesAmount = opdticketcharge;
-  late double rupeesAmountInt = OPdRupeesAmount.toDouble();
+  late int rupeesAmountInt = OPdRupeesAmount.toInt();
 
   num rupeesToPaisa(num rupees) {
     return rupees * 100.0;
