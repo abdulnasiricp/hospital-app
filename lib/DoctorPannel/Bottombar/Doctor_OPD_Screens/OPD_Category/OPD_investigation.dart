06492b2447664adaedde239350360887e87550e2
Abdul Nasir <abdulnasiricp@gmail.com>
2023-11-10 15:00:19 +0500

    OPD_investigation

--- lib/DoctorPannel/Bottombar/Doctor_OPD_Screens/OPD_Category/OPD_investigation.dart
+++ lib/DoctorPannel/Bottombar/Doctor_OPD_Screens/OPD_Category/OPD_investigation.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: sized_box_for_whitespace, unnecessary_string_interpolations, unnecessary_null_comparison
 
 import 'package:TezHealthCare/utils/colors.dart';
+import 'package:TezHealthCare/utils/mediaqury.dart';
 import 'package:flutter/material.dart';
 
 class OpdInvestigation extends StatefulWidget {
@@ -11,7 +12,7 @@ class OpdInvestigation extends StatefulWidget {
 }
 
 class _OpdInvestigationState extends State<OpdInvestigation> {
-   String selectedDiagnosisType = '';
+  List<String> selectedDiagnosisOptions = [];
   TextEditingController diagnosisController = TextEditingController();
 
   @override
@@ -25,112 +26,247 @@ class _OpdInvestigationState extends State<OpdInvestigation> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Diagnosis',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: double.infinity,
-              height: 40,
-              color: Colors.green[300],
-              child: const Center(
-                  child: Text(
-                'Diagnosis',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              )),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: double.infinity,
-              height: 40,
-              child:  Center(
-                  child:    InkWell(
-                        child: TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'This field is required';
-                        }
-                        return null;
-                      },
-                      readOnly:
-                          true, // Set this to true to disable the keyboard
-                      controller: diagnosisController,
-                      decoration: const InputDecoration(
-                      
-                        border: OutlineInputBorder(),
-                        hintText: 'Select options',
-                        fillColor: Colors.white,
-                        filled: true,
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Diagnosis',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: width / 2.2,
+                      height: 30,
+                      color: Colors.green[300],
+                      child: const Center(
+                          child: Text(
+                        'Diagnosis',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      )),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: width / 2.2,
+                      height: 30,
+                      child: Center(
+                        child: InkWell(
+                            child: TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'This field is required';
+                            }
+                            return null;
+                          },
+                          readOnly:
+                              true, // Set this to true to disable the keyboard
+                          controller: diagnosisController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Select options',
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                          onTap: () {
+                            selectDiagnosisOptions(context);
+                          },
+                        )),
                       ),
-                      onTap: () {
-                        selectDiagnosisOptions(context);
-                      },
-                    )),
-              ),
-            ),
-            SizedBox(height: 10,),
-             const Text(
-              'Pathology',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: double.infinity,
-              height: 40,
-              color: Colors.green[300],
-              child: const Center(
-                  child: Text(
-                'Test Name',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              )),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: double.infinity,
-              height: 40,
-              child:  Center(
-                  child:    InkWell(
-                        child: TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'This field is required';
-                        }
-                        return null;
-                      },
-                      readOnly:
-                          true, // Set this to true to disable the keyboard
-                      controller: diagnosisController,
-                      decoration: const InputDecoration(
-                      
-                        border: OutlineInputBorder(),
-                        hintText: 'Select options',
-                        fillColor: Colors.white,
-                        filled: true,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Pathology',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: width / 2.2,
+                      height: 30,
+                      color: Colors.green[300],
+                      child: const Center(
+                          child: Text(
+                        'Pathology',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      )),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: width / 2.2,
+                      height: 30,
+                      child: Center(
+                        child: InkWell(
+                            child: TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'This field is required';
+                            }
+                            return null;
+                          },
+                          readOnly:
+                              true, // Set this to true to disable the keyboard
+                          controller: diagnosisController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Select options',
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                          onTap: () {
+                            selectDiagnosisOptions(context);
+                          },
+                        )),
                       ),
-                      onTap: () {
-                        selectDiagnosisOptions(context);
-                      },
-                    )),
-              ),
-            )
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Other Test',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: width / 2.2,
+                      height: 30,
+                      color: Colors.green[300],
+                      child: const Center(
+                          child: Text(
+                        'Select Other Test',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      )),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: width / 2.2,
+                      height: 30,
+                      child: Center(
+                        child: InkWell(
+                            child: TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'This field is required';
+                            }
+                            return null;
+                          },
+                          readOnly:
+                              true, // Set this to true to disable the keyboard
+                          controller: diagnosisController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Select options',
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                          onTap: () {
+                            selectDiagnosisOptions(context);
+                          },
+                        )),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                
+              ],
+            ),
+            Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Radiology',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 30,
+                      color: Colors.green[300],
+                      child: Text(
+                        'Radiology',
+                        style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 30,
+                      child: Center(
+                        child: InkWell(
+                            child: TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'This field is required';
+                            }
+                            return null;
+                          },
+                          readOnly:
+                              true, // Set this to true to disable the keyboard
+                          controller: diagnosisController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Select options',
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                          onTap: () {
+                            selectDiagnosisOptions(context);
+                          },
+                        )),
+                      ),
+                    ),
+                  ],
+                ),
           ],
         ),
       ),
     );
   }
 
-    void selectDiagnosisOptions(BuildContext context) {
+  void selectDiagnosisOptions(BuildContext context) {
     showModalBottomSheet(
       isScrollControlled: true,
       shape: const RoundedRectangleBorder(
@@ -142,11 +278,11 @@ class _OpdInvestigationState extends State<OpdInvestigation> {
           builder: (BuildContext context, StateSetter setState) {
             // Create a list with "Emergency" and "General" only
             List<Map<String, dynamic>> combinedData = [
-              { "name": "Diagnosis 1"},
-              { "name": "Diagnosis 2"},
-              { "name": "Diagnosis 3"},
-              { "name": "Diagnosis 4"},
-              { "name": "Diagnosis 5"},
+              {"name": " 1"},
+              {"name": " 2"},
+              {"name": " 3"},
+              {"name": " 4"},
+              {"name": " 5"},
             ];
 
             return Container(
@@ -176,30 +312,42 @@ class _OpdInvestigationState extends State<OpdInvestigation> {
                     ],
                   ),
                   Expanded(
-                          child: ListView.builder(
-                            itemCount: combinedData.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              String item = combinedData[index]["name"];
-                              return Card(
-                                color: Colors.white70.withOpacity(0.7),
-                                child: ListTile(
-                                  title: Text(
-                                    '$item',
-                                    style:
-                                        const TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  onTap: () {
-                                   
-                                    selectedDiagnosisType = item;
-                                    diagnosisController.text =
-                                        '$selectedDiagnosisType';
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              );
+                    child: ListView.builder(
+                      itemCount: combinedData.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        String item = combinedData[index]["name"];
+                        bool isSelected =
+                            selectedDiagnosisOptions.contains(item);
+
+                        return Card(
+                          color: isSelected
+                              ? Colors.green
+                              : Colors.white70.withOpacity(0.7),
+                          child: ListTile(
+                            title: Text(
+                              '$item',
+                              style: TextStyle(
+                                fontWeight: isSelected
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                              ),
+                            ),
+                            onTap: () {
+                              setState(() {
+                                if (isSelected) {
+                                  selectedDiagnosisOptions.remove(item);
+                                } else {
+                                  selectedDiagnosisOptions.add(item);
+                                }
+                                diagnosisController.text =
+                                    selectedDiagnosisOptions.join(', ');
+                              });
                             },
                           ),
-                        ),
+                        );
+                      },
+                    ),
+                  ),
                 ],
               ),
             );
